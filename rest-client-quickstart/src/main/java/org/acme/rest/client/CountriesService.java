c1d77c974990ece9417d4f92fa4ddefe6dd0d0e2
Romain Quinio <romain.quinio@gmail.com>
2020-10-07 16:18:44 +0200

    Reproducer for Path and RegisterRestClient on separate interfaces

--- rest-client-quickstart/src/main/java/org/acme/rest/client/CountriesService.java
+++ rest-client-quickstart/src/main/java/org/acme/rest/client/CountriesService.java
@@ -1,33 +1,10 @@
 package org.acme.rest.client;
 
-import java.util.Set;
-import java.util.concurrent.CompletionStage;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import io.smallrye.mutiny.Uni;
+import javax.ws.rs.Path;
 
-@Path("/v2")
+@Path("/v2") // FIXME needs to be duplicated
 @RegisterRestClient
-public interface CountriesService {
-
-    @GET
-    @Path("/name/{name}")
-    @Produces("application/json")
-    Set<Country> getByName(@PathParam String name);
-
-    @GET
-    @Path("/name/{name}")
-    @Produces("application/json")
-    CompletionStage<Set<Country>> getByNameAsync(@PathParam String name);
-
-    @GET
-    @Path("/name/{name}")
-    @Produces("application/json")
-    Uni<Set<Country>> getByNameAsUni(@PathParam String name);
+public interface CountriesService extends CountriesApi {
 }
